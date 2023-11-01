0c35c934197ae8c0943ca7542bfb4e4f18b065f1
DESKTOP-M98ST0G\david <davidtheo@gmail.com>
2023-11-01 14:16:33 +0000

    start of the snake game

---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def move_snake(self):
+        for segments in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[segments - 1].xcor()
+            new_y = self.snake[segments - 1].ycor()
+            self.snake[segments].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            segment = Turtle(shape="square")
+            segment.penup()
+            segment.color("white")
+            segment.goto(position)
+            self.snake.append(segment)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
